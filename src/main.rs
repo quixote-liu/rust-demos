b2c394753711a48ed226c2162915407825323e98
lcs <lcs.shun@foxmail.com>
2023-05-17 23:27:27 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,15 @@ use ferris_says::say;
 use std::io::{ stdout, BufWriter };
 
 fn main() {
+    hello();
+    ferris_says_out();
+}
+
+fn hello() {
+    println!("hello, rust");
+}
+
+fn ferris_says_out() {
     let out = b"Hello fellow Rustaceans!";
     let width = 24;
     let mut writer = BufWriter::new(stdout());
